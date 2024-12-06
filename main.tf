5ffafce881a43fe677cb28bc22772cf6348c5bd0
Ragu-hub <raguilamathi1495@gmail.com>
2024-12-06 20:32:13 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "aws_organizations_account" "prod_account" {
+  name      = "prod-account"
+  email     = var.prod_email
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = var.ou_id
+  tags = {
+    Environment = "prod"
+    Owner       = "Drayboard"
+  }
+}
+
+resource "aws_organizations_account" "qa_account" {
+  name      = "qa-account"
+  email     = var.qa_email
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = var.ou_id
+  tags = {
+    Environment = "qa"
+    Owner       = "Drayboard"
+  }
+}
+
+resource "aws_organizations_account" "admin_account" {
+  name      = "shared-security-account"
+  email     = var.admin_email
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = var.ou_id
+  tags = {
+    Environment = "admin"
+    Owner       = "Drayboard"
+  }
+}
