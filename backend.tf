fea6a68301a2b0230b73fcee916a889eb59904d7
Ragu-hub <raguilamathi1495@gmail.com>
2024-12-13 14:49:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,5 +5,22 @@ terraform {
     region         = "us-east-2"                              
     encrypt        = true                                    
     acl            = "private"                                
+    dynamodb_table = "terraform-state-lock" # DynamoDB table name for state locking
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST" # On-demand capacity mode
+  hash_key       = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "Production"
+    Owner       = "Drayboard"
   }
 }
